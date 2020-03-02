f8e21d94b9d48130cfee4c74b2caef5cd6c9264d
Joe Bentley <jbentley@star.sr.bham.ac.uk>
2020-03-02 11:49:52 +0000

    Fix last commit CI

--- simba/__init__.py
+++ simba/__init__.py
@@ -5,5 +5,5 @@ from simba.core import \
     (is_transfer_matrix_physically_realisable,
      transfer_func_coeffs_to_state_space, transfer_function_to_state_space,
      transfer_function_to_coeffs, StateSpace,
-     concat, SLH, make_complex_ladder_state, split_two_dof)
+     concat, SLH, make_complex_ladder_state)
 from simba.errors import DimensionError, CoefficientError, StateSpaceError
